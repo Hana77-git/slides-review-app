6d873c74c48837dbd49a51bb28776302a5d4842c
Hana77-git <hanak2027102@gmail.com>
2025-06-21 12:48:02 +0000

    use image source searcher

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ from io import BytesIO
 from pathlib import Path
 
 from utils.pptx_reader import extract_text_and_images
-from utils.question_generator import generate_question
-# from utils.image_search import search_image_sources
+# from utils.question_generator import generate_question
+from utils.image_search import search_image_sources
 
 # タイトル
 st.title("🎓 発表スライド評価アシスタント")
@@ -27,8 +27,8 @@ if uploaded_file:
     extract_text_and_images(uploaded_file, extract_dir)
 
     st.header("📊 スライド解析結果")
-    slides = list((extract_dir / "text").glob("*.txt"))
-    images = list((extract_dir / "img").glob("*.png"))
+    slides = sorted(list((extract_dir / "text").glob("*.txt")))
+    images = sorted(list((extract_dir / "img").glob("*.png")))
 
     for i, slide in enumerate(slides):
       with open(slide, "r", encoding="utf-8") as f:
@@ -39,29 +39,14 @@ if uploaded_file:
       st.text(slide_text)
 
       st.markdown("**❓ 想定される質問：**")
-      question = generate_question(slide_text)
-      st.text(question)
+      # question = generate_question(slide_text)
+      # st.text(question)
       
     for i, image in enumerate(images):
       st.subheader(f"Image {i+1}")
       st.image(image, caption=image, width=300)
-
-    # for slide_num, data in slide_data.items():
-    #     st.subheader(f"📄 Slide {slide_num}")
-
-    #     st.markdown("**📝 スライド内容：**")
-    #     st.text(data["text"])
-
-    #     st.markdown("**❓ 想定される質問：**")
-    #     questions = generate_questions(data["text"])
-    #     for q in questions:
-    #         st.markdown(f"- {q}")
-
-        # if data["images"]:
-        #     st.markdown("**🖼️ 画像と出典推定：**")
-        #     sources = search_image_sources(data["images"])
-        #     for img_path in data["images"]:
-        #         st.image(img_path, caption=os.path.basename(img_path), width=300)
-        #         st.markdown(f"🔗 出典（推定）：{sources.get(img_path, '見つかりませんでした')}")
-
-        # st.markdown("---")
+    
+    sources = search_image_sources(images)
+    for key, value in sources.items():
+      st.write(f"**{key}**:")
+      st.write(f" 🔗 出典（推定）{value}")
